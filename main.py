02ce175d43dd182b6923233a30b7cfe0731b74f2
Jackie Li <jli38@stuy.edu>
2019-02-10 23:12:49 -0500

    Let's try this

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import random
 screen = new_screen()
 color = [ 255, 255, 0 ]
 
-ran = random.randint(0, 255)
 
-draw_line(ran, ran, ran, ran, screen, [ran, ran, ran])
-draw_line(ran, ran, ran, ran, screen, [ran, ran, ran])
-draw_line(ran, ran, ran, ran, screen, [ran, ran, ran])
-draw_line(ran, ran, ran, ran, screen, [ran, ran, ran])
-draw_line(ran, ran, ran, ran, screen, [ran, ran, ran])
-
+draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 500) % 256, random.randint(0, 500) % 256, random.randint(0, 500) % 256])
+draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 500) % 256, random.randint(0, 500) % 256, random.randint(0, 500) % 256])
+draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 500) % 256, random.randint(0, 500) % 256, random.randint(0, 500) % 256])
+draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 500) % 256, random.randint(0, 500) % 256, random.randint(0, 500) % 256])
+draw_line(random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), random.randint(0, 500), screen, [random.randint(0, 500) % 256, random.randint(0, 500) % 256, random.randint(0, 500) % 256])
 
 
 display(screen)
