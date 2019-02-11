c99a8c6f2ec8b41ebeb002c679090682a452f577
Jackie Li <jli38@stuy.edu>
2019-02-10 22:50:34 -0500

    Cleaned up testing

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
 from display import *
 from draw import *
-from subprocess import Popen, PIPE
-import random
 
 screen = new_screen()
 color = [ 255, 255, 0 ]
 
-for i in range(0,500,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( i, 0, i, 500, screen, color)
 
-for i in range(0,500,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, i, 500, i, screen, color)
-
-
-######
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, i, i, 0, screen, color)
-
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, i, (i+random.randrange(100))*2, 0, screen, color)
-
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, i, (i-random.randrange(100))*0.5 , 0, screen, color)
-
-######
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, 500-i, i, 500, screen, color)
-
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-    draw_line( 0, 500-i, (i+random.randrange(100))*2, 500, screen, color)
-
-for i in range(0,1000,100):
-    color = [ (i+random.randrange(100)%256), (i+random.randrange(100))%256, (i+random.randrange(100))%256]
-draw_line( 0, 500-i, (i-random.randrange(100))*0.5 , 500, screen, color)
+draw_line(50, 300, 200, 300, screen, [0, 255, 255])
+draw_line(50, 100, 200, 100, screen, [0, 255, 255])
+draw_line(50, 100, 200, 300, screen, [0, 255, 255])
+draw_line(50, 200, 200, 200, screen, [0, 255, 255])
 
 
 display(screen)
