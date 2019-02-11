c273ec37aa8a05b3c4fc87f143e629e00872ace5
jli38 <jli38@stuy.edu>
2019-02-10 22:34:35 -0500

    hihihi

--- draw.py
+++ draw.py
@@ -1,4 +1,65 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+
+    a = (y1 - y0)
+    b = -1 * (x1 -x0)
+
+    if (y0 <= y1):
+	#If slope <= 1
+
+	#Octant 1
+	if (abs(a) <= abs(b)):
+	    d = 2 * a + b
+
+	    while (x0 <= x1):
+		plot(screen, color, x0, y0)
+		if d > 0:
+		    y0 += 1
+		    d += 2*b
+
+		x0 += 1
+		d += 2*a
+
+	else:
+	    #Octant 2
+	    d = a + 2 * b
+
+	    while (y0 <= y1):
+
+		plot(screen, color, x0, y0)
+		if (d < 0):
+		    x0 += 1
+		    d += 2 * a
+
+		y0 += 1
+		d += 2 * b
+
+    else:
+        if(abs(a) <= abs(b)): #If slope <= 1
+            #Octant 8
+            d = 2 * a - b
+
+            while x0 <= x1:
+
+                #PLOT
+                plot(screen, color, x0, y0)
+                if (d < 0):
+                    y0 -= 1
+                    d -= 2 * b
+
+                x0 += 1
+                d += 2 * a
+        else:
+            #Octant 7
+            d = a - 2 * b
+
+            while (y0 >= y1):
+
+                plot(screen, color, x0, y0)
+                if d > 0:
+                    x0 += 1
+                    d += 2 * a
+
+                y0 -= 1
+		d -= 2 * b
--- main.py
+++ main.py
@@ -2,7 +2,41 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 0 ]
+
+
+# Q1
+draw_line(0, 0, 250, 0, screen, color)
+draw_line(0, 0, 250, 250, screen, color)
+draw_line(0, 0, 250, 100, screen, color)
+
+# Q2
+draw_line(0, 0, 0, 250, screen, color)
+draw_line(0, 0, 250, 350, screen, color)
+
+# Q3
+draw_line(-250, 350, 0, 0, screen, color)
+
+# Q4
+draw_line(-250, 0, 0, 0, screen, color)
+draw_line(-250, 250, 0, 0, screen, color)
+draw_line(-250, 100, 0, 0, screen, color)
+
+# Q5
+draw_line(-250, -100, 0, 0, screen, color)
+draw_line(-250, -250, 0, 0, screen, color)
+
+# Q6
+draw_line(-250, -350, 0, 0, screen, color)
+draw_line(0, -250, 0, 0, screen, color)
+
+# Q7 
+draw_line(0, 0, 250, 350, screen, color)
+
+# Q8
+draw_line(0, 0, 250, -250, screen, color)
+draw_line(0, 0, 250, -100, screen, color)
+
 
 display(screen)
 save_extension(screen, 'img.png')
